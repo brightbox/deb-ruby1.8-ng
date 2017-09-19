d197ab1cedab791fe94a51744da3408e1a7abf2b
John Leach <john@johnleach.co.uk>
2017-09-19 17:50:38 +0100

    CVE-2017-10784 WEBrick Basic auth injection vuln

--- lib/webrick/httpstatus.rb
+++ lib/webrick/httpstatus.rb
@@ -13,10 +13,6 @@ module WEBrick
   module HTTPStatus
 
     class Status      < StandardError
-      def initialize(*args)
-        args[0] = AccessLog.escape(args[0]) unless args.empty?
-        super(*args)
-      end
       class << self
         attr_reader :code, :reason_phrase
       end
